deb3cac05b8b96cce7ad6ddb430718ce10f832a8
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2021-04-09 20:44:56 -0300

    Added click count route and a few response improvements

--- api/methods.py
+++ api/methods.py
@@ -15,33 +15,30 @@ def get_last_url_id():
     return last_url.id
 
 
-def generate_hash():
-    """
-    Generates a new short hash using the next URL ID to be added to
-    the database.
-    """
-    last_id = get_last_url_id()
-    new_hash = hashmaker.encode(last_id + 1)
+# def generate_hash():
+#     """
+#     Generates a new short hash using the next URL ID to be added to
+#     the database.
+#     """
+#     last_id = get_last_url_id()
+#     new_hash = hashmaker.encode(last_id + 1)
+#
+#     return new_hash
 
-    return new_hash
 
-
-def generate_hash_with_id(id):
+def generate_hash(url_id):
     """
     Generates a new short hash using an URL's ID, making it unique.
     """
-    new_hash = hashmaker.encode(id)
+    new_hash = hashmaker.encode(url_id)
 
     return new_hash
 
 
-def get_full_url(hashed_id):
+def get_url_by_hash(hashed_id):
     """
     Returns the full URL corresponding to the hash passed as argument.
     """
     url_instance = Url.query.filter_by(hash=hashed_id).first()
 
-    if url_instance:
-        return url_instance.long_url
-
-    return None
+    return url_instance if url_instance else None
--- api/routes.py
+++ api/routes.py
@@ -2,20 +2,15 @@ import json
 from flask import Blueprint, request, redirect, make_response
 from api.database import db_session
 from api.models import Url
-from api.methods import generate_hash_with_id, get_full_url
+from api.methods import generate_hash, get_url_by_hash
 from api.validators import validate_url
 
 
 api = Blueprint('api', __name__, url_prefix='/lil')
 
 
-@api.route('/hello')
-def hello():
-    return 'Hello, World!'
-
-
 @api.route('/short-url', methods=['POST'])
-def short_url():
+def add_url():
     print(request.url_root)
     data = request.json
     long_url = data.get('url', None)
@@ -31,23 +26,23 @@ def short_url():
         print(new_url.serialize())
 
         # Create new hash using new URL's id and update instance
-        new_hash = generate_hash_with_id(new_url.id)
+        new_hash = generate_hash(new_url.id)
         new_url.hash = new_hash
         print(new_url.serialize())
         db_session.add(new_url)
         db_session.commit()
 
-        # Serialize instance
-        serialized_data = new_url.serialize()
+        data = {
+            'short_url': f'{request.url_root}/lil/{new_url.hash}'
+        }
 
-        # Return serialized data
-        return make_response(json.dumps(serialized_data), 201)
+        return make_response(data, 201)
 
     error_msg = 'Sorry, we couldn\'t shorten the URL. Please ensure the URL begins' \
                 ' with \'https://\' or \'http://\', and has a valid format. Examples:' \
                 ' \'https://my-url.com\', \'https://www.my-url.com\'.'
 
-    return make_response({"error": error_msg}, 400)
+    return make_response({'error': error_msg}, 400)
 
 
 @api.route('/<string:hashed_id>', methods=['GET'])
@@ -56,35 +51,43 @@ def redirect_url(hashed_id):
     Redirection route. Redirects request to a short URL to the
     full URL.
     """
-    url = get_full_url(hashed_id)
-    print(url)
-    print(bool(url))
+    url = get_url_by_hash(hashed_id)
     if url:
-        print("fired if")
-        return redirect(url)
+        # Increment URL access counter before redirecting
+        url.clicks = url.clicks + 1
+        db_session.add(url)
+        db_session.commit()
 
-    return 'redirect url'
+        return redirect(url.long_url)
+
+    error_msg = 'Sorry, the requested short URL was not found.'
+    return make_response({'error': error_msg}, 404)
 
 
-@api.route('get-db', methods=['GET'])
-def get_db():
+@api.route('/<string:hashed_id>/clicks', methods=['GET'])
+def get_clicks(hashed_id):
+    """
+    Click counting route. Returns how many times a short URL was
+    visited.
+    """
+    url = get_url_by_hash(hashed_id)
+    if url:
+        data = {
+            'clicks': url.clicks,
+            'message': f'This short URL has been accessed {url.clicks} times.'
+        }
+
+        return make_response(data, 200)
+
+    error_msg = 'Sorry, the requested short URL was not found.'
+    return make_response({'error': error_msg}, 404)
+
+
+@api.route('get-all', methods=['GET'])
+def get_all():
     """
     Dev route. Returns all URLs in database.
     """
     urls = Url.query.all()
-    print(urls)
+
     return json.dumps(Url.serialize_list(urls))
-
-
-@api.route('post-db', methods=['POST'])
-def post_db():
-    data = request.json
-    long_url = data.get('url', None)
-    if long_url is not None:
-        new_url = Url(long_url=long_url, hash='shortio')
-        db_session.add(new_url)
-        db_session.commit()
-
-        return json.dumps(new_url.short_url)
-
-    return 'Error'
